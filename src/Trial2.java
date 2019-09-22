682cb5139902325ca357048147c919b454af08d2
karthik-kumar08 <kumarskkarthik@gmail.com>
2019-09-21 21:46:57 -0600

    added java nature to project and added Trial2

---
+++ src/Trial2.java
@@ -0,0 +1,18 @@
+
+public class Trial2 {
+
+	public static void main(String[] args) {
+		System.out.println("The Sum of two numbers is:");
+		int a = 10;
+		int b = 5;
+
+		System.out.println(sum(a, b));
+
+	}
+
+	public static int sum(int a, int b) {
+		return a + b;
+
+	}
+}
+
